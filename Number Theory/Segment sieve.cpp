4fa06218e0775970b20b60abe3d8ced04075f1c9
srlabib <71954819+srlabib@users.noreply.github.com>
2024-06-09 13:51:08 +0600

    Update Segment sieve.cpp

--- Number Theory/Segment sieve.cpp
+++ Number Theory/Segment sieve.cpp
@@ -1,53 +1,68 @@
 #include<bits/stdc++.h>
+ 
+ using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+#define All(a) a.begin(),a.end()
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+template<typename T> void get_vector(T&a){for(auto&e:a)cin>>e;}
+template<typename T> void put_vector(T a){for(auto e:a)cout<<e<<" ";cout<<endl;}
+const int MAX = 46500; //max value is the squar root of the maxumum value of b
+bool isprime[MAX+5];
+vi primes;
 
-using namespace std;
 
-void segment_sieve(long long a, long long b){
-    int N = sqrt(b+1);
-    vector<long long> primes;
-    vector<bool> check(N+1,1);
-    check[1] = 0;
+void sieve(){ // getting all the primes up to sqrt(b)
+    for(int i = 3; i<MAX; i+=2)isprime[i] = 1;
+    isprime[2] = 1;
     primes.push_back(2);
-    for(int i = 4; i<=N; i+=2)check[i] = 0;
-    for(int i = 3; i<=N; i+=2){
-        if(check[i]){
-            primes.push_back(i);
-            if(i*i>N)continue;
-            for(int j = i*i; j<=N; j+=2*i){
-                check[j] = 0;
-            }
-        }
+    for(ll i = 3; i<MAX; i+=2){
+        if(isprime[i])primes.push_back(i);
+        if(i*i>MAX)continue;
+        for(ll j = i*i; j<MAX; j+=i)isprime[j] = 0;
     }
-    int n2 = primes.size();
-    
-    vector<bool>check2(b-a+1,1);
-    for(int i = 0; i<n2; i++){
-        int f = a%primes[i]==0?a/primes[i]:a/primes[i]+1;
-        for(int j = f*primes[i]-a; j<=b-a; j+=primes[i]){
-            check2[j] = 0;
-        }
-    }
-    int cnt = 0;
-    for(int i = 0; i<=b-a; i++){
-        if(check2[i]){
-            cout<<i+a<<" ";
-            cnt++;
-        }
-    }
-    cout<<endl<<endl<<cnt<<endl;
 }
+void solve()
+{
+    ll a,b;
+    static int cs = 0;
+    cin>>a>>b;
+
+    vector<bool>P(b-a+5);
+    for(ll i = a+(a%2==0); i<=b; i+=2)P[i-a] = 1;
+    if(2-a>=0)P[2-a] = 1; //if 2 is in the range setting 2 as prime
+    if(1-a>=0)P[1-a] = 0;  //if 1 is in the range setting 1 as not prime 
+    for(ll e: primes){
+        if(e>b)break;
+        ll x;
+        if(e>=a)x = e+e; // if prime is in the range, we should not mark it as not prime in the loop
+        else if(a%e==0)x = a;
+        else x = (a/e+1)*e; 
+        for(ll i = x; i<=b; i+=e){
+            P[i-a]=0;
+        }
+    }
+    int res = 0;
+
+    for(ll i = a; i<=b; i++)res+=P[i-a];
+    cout << "Case "<<++cs<<": "<<res<<endl;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    sieve();
+
+
     int T;
     cin >> T;
     while(T--)
-    {
-        int a,b;
-        cin>>a>>b;
-        segment_sieve(a,b);
-    }
+        solve();
     return 0;
 }
