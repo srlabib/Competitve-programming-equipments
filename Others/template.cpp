6799c3c2e223d63c9896d670d41546316d3d962a
srlabib <71954819+srlabib@users.noreply.github.com>
2024-09-30 02:34:11 +0600

    Create template.cpp

---
+++ Others/template.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set; // find_by_order, order_of_key
+
+using namespace std; 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<ll,ll> pll;
+typedef vector<pll> vpll;
+typedef vector<ll> vl;
+
+#define All(a) a.begin(),a.end()
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+template<typename T> void get_vector(T&a){for(auto&e:a)cin>>e;}
+template<typename T> void put_vector(T a){for(auto e:a)cout<<e<<" ";cout<<endl;}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    //start coding here
+    return 0;
+}
