e2e9695a3d5bb3ba9e62e46ecf1a3b52d4d8b1f7
Shahtaz Rahman Labib <71954819+srlabib@users.noreply.github.com>
2024-10-30 23:46:10 +0600

    Update PrimeFactorization.cpp

--- Number Theory/PrimeFactorization.cpp
+++ Number Theory/PrimeFactorization.cpp
@@ -1,136 +1,64 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-#define abs_val(a)       (((a)>0)?(a):-(a))
-typedef long long        ll;
 
-map <ll, ll> MAP;
-ll mulMod(ll a, ll b, ll c)
-{
-    ll x = 0, y = a%c;
-    while (b > 0)
-    {
-        if (b%2 == 1)
-            x = (x+y)%c;
-        y = (y*2)%c;
-        b /= 2;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+
+namespace Rho {
+    ULL mult(ULL a, ULL b, ULL mod) {
+        LL ret = a * b - mod * (ULL)(1.0L / mod * a * b);
+        return ret + mod * (ret < 0) - mod * (ret >= (LL) mod);
     }
-    return x%c;
-}
- 
-ll gcd(ll a, ll b)
-{
-     return !b ? a : gcd(b, a%b);
-}
- 
-ll Mod(ll a, ll b, ll p)
-{
-    ll res = 1;
-    ll x = a%p;
- 
-    while (b)
-    {
-        if (b&1)
-        {
-            res = mulMod(res, x, p);
+
+    ULL power(ULL x, ULL p, ULL mod){
+        ULL s=1, m=x;
+        while(p) {
+            if(p&1) s = mult(s, m, mod);
+            p>>=1;
+            m = mult(m, m, mod);
         }
-        x = mulMod(x, x, p);
-        b /= 2;
+        return s;
     }
-    return res%p;
-}
- 
- 
-bool millerTest(ll d, ll s, ll n)
-{
-    ll a = rand() % (n-4) + 2;
-    ll x = Mod(a, s, n);
- 
-    if (x==1 or x==n-1)
-        return true;
- 
-    for (int r = 1; r < d; r++)
-    {
-        x = mulMod(x, x, n);
- 
-        if (x==1)
-            return false;
-        if (x==n-1)
-            return true;
-    }
-    return false;
-}
- 
- 
- 
-bool isPrime(ll n, int k = 20)
-{
-    if (n <= 1 || n==4)
-        return false;
-    if (n<=3)
-        return true;
-    if (n%2==0)
-        return false;
- 
-    ll s = n-1, d = 0;
-    while (s%2==0)
-    {
-        s/=2;
-        d++;
-    }
-    for (int i = 0; i < k; i++)
-    {
-        if (millerTest(d, s, n)==false)
-            return false;
-    }
-    return true;
-}
- 
- 
-ll pollard_rho(ll n)
-{
-    if(n%2==0)
-        return 2;
- 
-    ll x = rand()%n+1;
-    ll c = rand()%n+1;
-    ll y = x;
-    ll g = 1;
- 
-    while(g==1)
-    {
-        x = (mulMod(x, x, n) + c)%n;
-        y = (mulMod(y, y, n) + c)%n;
-        y = (mulMod(y, y, n) + c)%n;
-        g = gcd(abs_val(x-y),n);
-    }
-    return g;
-}
 
-void factorize(ll n)
-{
-    if(n == 1)
-        return;
- 
-    ll divisor = pollard_rho(n);
-	if(isPrime(n))      
-    {
-        MAP[n]++;
-        return;
-    }   
-    factorize(divisor);
-    factorize(n/divisor);
-}
- 
-int main()
-{
-	ll n;
-    cin >> n;
-	factorize(n);
-    for(auto e:MAP)
-    {
-        cout << e.first<<" " <<e.second<<endl;
+    vector<LL> bases = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+    bool isprime(LL n) {
+        if (n<2)    return 0;
+        if (n%2==0)   return n==2;
+
+        ULL s = __builtin_ctzll(n-1), d = n>>s;
+        for (ULL x: bases) {
+            ULL p = power(x%n, d, n), t = s;
+            while (p!=1 && p!=n-1 && x%n && t--) p = mult(p, p, n);
+            if (p!=n-1 && t != s)               return 0;
+        }
+        return 1;
+    }
+
+    ///Returns a proper divisor if n is composite, n otherwise
+    ///Possible Optimization: use binary gcd for ~10% speedup
+    mt19937_64 rng(chrono::system_clock::now().time_since_epoch().count());
+    ULL FindFactor(ULL n) {
+        if (n == 1 || isprime(n))    return n;
+        ULL c = 1, x = 0, y = 0, t = 0, prod = 2, x0 = 1, q;
+        auto f = [&](ULL X) { return mult(X, X, n) + c;};
+
+        while (t++ % 128 or gcd(prod, n) == 1) {
+            if (x == y) c = rng()%(n-1)+1, x = x0, y = f(x);
+            if ((q = mult(prod, max(x, y) - min(x, y), n))) prod = q;
+            x = f(x), y = f(f(y));
+        }
+        return gcd(prod, n);
+    }
+
+    ///Returns all prime factors
+    vector<ULL> factorize(ULL x) {
+        if (x == 1)     return {};
+        ULL a = FindFactor(x), b = x/a;
+        if (a == x) return {a};
+        vector<ULL> L = factorize(a), R = factorize(b);
+        L.insert(L.end(), R.begin(), R.end());
+        return L;
     }
 }
- 
